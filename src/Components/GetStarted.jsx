c685e5ada5887a70ee4fb0cc81bb4aff203524a3
Adeloba Joshua <adelobajoshua19@gmail.com>
2023-10-03 18:26:25 +0100

    first commit

---
+++ src/Components/GetStarted.jsx
@@ -0,0 +1,62 @@
+import { Button, styled, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import React from "react";
+import homeIllustration from "../media/illustration.png";
+import CustomButton from "./CustomButton";
+
+const GetStarted = () => {
+  const CustomContainer = styled(Container)(({ theme }) => ({
+    backgroundColor: "#17275f",
+    height: "416px",
+    borderRadius: "15px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    [theme.breakpoints.down("md")]: {
+      height: "auto",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: theme.spacing(3, 3, 0, 3),
+      width: "90%"
+    }
+  }));
+
+  const CustomBox = styled(Box)(({ theme }) => ({
+    padding: theme.spacing(10, 0, 10, 0),
+    margin: theme.spacing(0, 2, 0, 2),
+    [theme.breakpoints.down("md")]: {
+      padding: "0"
+    }
+  }));
+
+  return (
+    <Container>
+      <CustomContainer>
+        <Box>
+          <Typography
+            sx={{ fontSize: "35px", color: "white", fontWeight: "700" }}
+          >
+            Featured Properties
+          </Typography>
+          <Typography
+            sx={{ fontSize: "16px", color: "#ccc", fontWeight: "500", my: "3", padding: "10px" }}
+          >
+            Everything you need to know about house
+          </Typography>
+          <CustomButton 
+          backgroundColor="#fff"
+          color="#17275f"
+          buttonText="Get Started Now"
+          getStartedBtn={true}
+          />
+        </Box>
+        <img src={homeIllustration}
+        alt="illustration"
+        style={{maxWidth: "100%"}}
+        />
+      </CustomContainer>
+    </Container>
+  );
+};
+
+export default GetStarted;
